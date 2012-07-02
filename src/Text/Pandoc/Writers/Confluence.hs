7c26d4e6bfe08cf8f16770bbbd698b18947ff70a
Gavin Beatty <gavinbeatty@gmail.com>
2012-07-02 21:00:51 +0200

    another wip: broken after i removed stUseTags!
    stUseTags is pretty useless since Confluence actually _doesn't_ support raw
    html outside of {html} macro (which comes from a plugin).

--- src/Text/Pandoc/Writers/Confluence.hs
+++ src/Text/Pandoc/Writers/Confluence.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DoAndIfThenElse #-}
 {-
 Copyright (C) 2006-2010 John MacFarlane <jgm@berkeley.edu>
 Copyright (C) 2012 Gavin Beatty <gavinbeatty@gmail.com>
@@ -29,27 +30,37 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 Conversion of 'Pandoc' documents to Confluence markup.
 
 Confluence:  <https://confluence.atlassian.com/display/DOC/Confluence+Wiki+Markup>
+
+Problems with the Confluence markup format:
+1.  Nested macros are not supported:
+    * Macros are used for: blockquotes, code blocks, pre-formatted blocks,
+      footnotes, math.
+2.  Raw HTML is only supported with the HTML plugin+macro.
+3.  .
 -}
 module Text.Pandoc.Writers.Confluence ( writeConfluence ) where
 import Text.Pandoc.Definition
 import Text.Pandoc.Shared
 import Text.Pandoc.Templates (renderTemplate)
 import Data.List ( intersect, intercalate )
---import Data.Char ( isUpper, isLower, isDigit )
 --import Network.URI ( escapeURIString, isUnescapedInURI )
 import Control.Monad.State
 
 data WriterState = WriterState {
   stListLevel       :: Int            -- How many nested levels. Top level => 0
-  , stUseTags       :: Bool           -- True if we should use HTML tags because we're in a complex list
-  , stHaveFootnotes :: Bool           -- Whether there are footnotes
+  , stHaveFootnotes :: Bool           -- Whether there are footnotes to display
+  , stMacros        :: [String]       -- List of macros we're inside
   }
 
 -- | Convert Pandoc to Confluence.
 writeConfluence :: WriterOptions -> Pandoc -> String
 writeConfluence opts document =
   evalState (pandocToConfluence opts document)
-            (WriterState { stListLevel = 0, stUseTags = False, stHaveFootnotes = False })
+            (WriterState {
+                stListLevel = 0
+                , stHaveFootnotes = False
+                , stMacros = []
+                })
 
 -- | Return Confluence representation of document.
 pandocToConfluence :: WriterOptions -> Pandoc -> State WriterState String
@@ -67,21 +78,17 @@ pandocToConfluence opts (Pandoc _ blocks) = do
 
 -- | Escape special characters for Confluence.
 escapeString :: String -> String
-escapeString s = s
-{-
-  let (pre,as) = break (=='{') s
-  in if not $ null as
-        then let (inside,bs) = break (=='>') as
-             in if not $ null bs
-                   then pre ++ "{{{" ++ inside ++ ">}}}" ++ tail bs
-                   else s
-        else s
--}
+escapeString = escapeStringUsing [
+                   ('{', "\\{")
+                   , ('}', "\\}")
+                   ]
 
+-- | Footnotes require the following free (after filling in survey...) plugin:
+-- http://www.adaptavist.com/display/Plugins/Footnotes
 displayFootnotesToConfluence :: WriterOptions -> State WriterState String
 displayFootnotesToConfluence _ = do
   footnotes <- get >>= return . stHaveFootnotes
-  return $ if null footnotes then "\n" else "{display-footnotes}\n"
+  return $ if footnotes then "{display-footnotes}" else ""
 
 -- | Convert Pandoc block element to Confluence.
 blockToConfluence :: WriterOptions -- ^ Options
@@ -97,7 +104,7 @@ blockToConfluence opts (Para [Image txt (src,tit)]) = do
   let attrTitle = if not $ null tit
                      then "title=\"" ++ dqAmpEscapeString tit ++ "\""
                      else if not $ null txt
-                             then " title=\"" ++ dqAmpEscapeString capt ++ "\""
+                             then "title=\"" ++ dqAmpEscapeString capt ++ "\""
                              else ""
       attrAlt = if not $ null txt
                    then "alt=\"" ++ dqAmpEscapeString capt ++ "\""
@@ -119,14 +126,17 @@ blockToConfluence _ (RawBlock "confluence" str) = return str
 blockToConfluence _ (RawBlock "html" str) = return str
 blockToConfluence _ (RawBlock _ _) = return ""
 
-blockToConfluence _ HorizontalRule = return "\n----\n"
+blockToConfluence _ HorizontalRule = return "----\n"
 
 blockToConfluence opts (Header level inlines) = do
   -- Before a new header, flush the footnotes.
   displayFootnotes <- displayFootnotesToConfluence opts
   modify $ \s -> s { stHaveFootnotes = False }
   contents <- inlineListToConfluence opts inlines
-  return $ displayFootnotes ++ "\nh" ++ (show level) ++ ". " ++ contents ++ "\n"
+  let prefix = if null displayFootnotes
+                  then ""
+                  else displayFootnotes ++ "\n"
+  return $ prefix ++ "h" ++ (show level) ++ ". " ++ contents ++ "\n"
 
 blockToConfluence _ (CodeBlock (_,classes,_) str) = do
   -- XXX what does confluence code support
@@ -143,61 +153,20 @@ blockToConfluence _ (CodeBlock (_,classes,_) str) = do
   return $ "{code" ++ fmt ++ "}\n" ++ str ++ "\n{code}\n"
 
 blockToConfluence opts (BlockQuote blocks) = do
+  inQuote <- get >>= return . stInQuote
+  modify $ \s -> s { stInQuote = True }
   contents <- blockListToConfluence opts blocks
-  return $ "{quote}\n" ++ contents ++ "\n{quote}"
+  modify $ \s -> s { stInQuote = inQuote }
+  return $ "{quote}\n" ++ init contents ++ "{quote}\n"
 
-blockToConfluence opts t@(Table _ _ widths _ _) = do
+blockToConfluence opts t@(Table _ _ _ _ _) = do
   useTags <- get >>= return . stUseTags
-  if not . all (== 0.0) widths then
+  if not $ isSimpleTable t then do
       modify $ \s -> s { stUseTags = True }
-      table <- tableToConfluence opts t
+      table <- tableToConfluenceUsingTags opts t
       modify $ \s -> s { stUseTags = useTags }
-      table
-  else tableToConfluence opts t
-
-tableToConfluence opts (Table capt aligns widths headers rows) = do
-  useTags <- get >>= return . stUseTags
-  if useTags then
-      let alignStrings = map alignmentToString aligns
-      captionDoc <- if null capt
-                       then return ""
-                       else do
-                          c <- inlineListToConfluence opts capt
-                          return $ "<caption>" ++ c ++ "</caption>\n"
-      let percent w = show (truncate (100*w) :: Integer) ++ "%"
-      let coltags = if all (== 0.0) widths
-                       then ""
-                       else unlines $ map
-                             (\w -> "<col width=\"" ++ percent w ++ "\" />") widths
-      head' <- if all null headers
-                  then return ""
-                  else do
-                     hs <- tableRowToConfluence opts alignStrings 0 headers
-                     return $ "<thead>\n" ++ hs ++ "\n</thead>\n"
-      body' <- zipWithM (tableRowToConfluence opts alignStrings) [1..] rows'
-      return $ "<table>\n" ++ captionDoc ++ coltags ++ head' ++
-                "<tbody>\n" ++ unlines body' ++ "</tbody>\n</table>\n"
-  else
-      let alignStrings = map alignmentToString aligns
-      let summary = if null capt
-                       then ""
-                       else do
-                          c <- inlineListToConfluence opts capt
-                          $ "summary=\"" ++ dqAmpEscapeString c ++ "\""
-      let coltags = if all (== 0.0) widths
-                       then ""
-                       else unlines $ map
-                             (\w -> "<col width=\"" ++ percent w ++ "\" />") widths
-      head' <- if all null headers
-                  then return ""
-                  else do
-                     hs <- tableRowToConfluence opts alignStrings 0 headers
-                     return $ "<thead>\n" ++ hs ++ "\n</thead>\n"
-      body' <- zipWithM (tableRowToConfluence opts alignStrings) [1..] rows'
-      return $ "<table>\n" ++ captionDoc ++ coltags ++ head' ++
-                "<tbody>\n" ++ unlines body' ++ "</tbody>\n</table>\n"
-
-blockToConfluence opts (Table capt aligns widths headers rows') = do
+      return table
+  else tableToConfluenceUsingBars opts $ mkSimpleTable t
 
 blockToConfluence opts x@(BulletList items) = do
   oldUseTags <- get >>= return . stUseTags
@@ -210,9 +179,9 @@ blockToConfluence opts x@(BulletList items) = do
         modify $ \s -> s { stUseTags = oldUseTags }
         return $ "<ul>\n" ++ unlines contents ++ "</ul>\n"
      else do
-        modify $ \s -> s { stListLevel = (stListLevel s) + 1 }
+        modify $ \s -> s { stListLevel = listLevel + 1 }
         contents <- mapM (listItemToConfluence opts '*') items
-        modify $ \s -> s { stListLevel = (stListLevel s) - 1 }
+        modify $ \s -> s { stListLevel = listLevel - 1 }
         return $ unlines contents ++ if 0 == listLevel then "\n" else ""
 
 blockToConfluence opts x@(OrderedList attribs items) = do
@@ -226,9 +195,9 @@ blockToConfluence opts x@(OrderedList attribs items) = do
         modify $ \s -> s { stUseTags = oldUseTags }
         return $ "<ol" ++ listAttribsToString attribs ++ ">\n" ++ unlines contents ++ "</ol>\n"
      else do
-        modify $ \s -> s { stListLevel = (stListLevel s) + 1 }
+        modify $ \s -> s { stListLevel = listLevel + 1 }
         contents <- mapM (listItemToConfluence opts '#') items
-        modify $ \s -> s { stListLevel = (stListLevel s) - 1 }
+        modify $ \s -> s { stListLevel = listLevel - 1 }
         return $ unlines contents ++ if 0 == listLevel then "\n" else ""
 
 blockToConfluence opts (DefinitionList items) = do
@@ -237,6 +206,50 @@ blockToConfluence opts (DefinitionList items) = do
   contents <- mapM (definitionListItemToConfluence opts) items
   modify $ \s -> s { stUseTags = oldUseTags }
   return $ "<dl>\n" ++ unlines contents ++ "</dl>\n"
+
+isSimpleTable :: Block -> Bool
+isSimpleTable (Table capt aligns widths _ _) =
+  noCaption && allZeroWith && allSimpleAlign
+  where allZeroWith    = all (== 0.0) widths
+        allSimpleAlign = all (\a -> a == AlignLeft || a == AlignDefault) aligns
+        noCaption      = null capt
+isSimpleTable _ = False
+
+mkSimpleTable :: Block -> Block
+mkSimpleTable (Table capt _ _ headers rows) = Table capt [] [] headers rows
+mkSimpleTable x = x
+
+tableToConfluenceUsingTags :: WriterOptions -> Block -> State WriterState String
+tableToConfluenceUsingTags opts (Table capt aligns widths headers rows) = do
+  let alignStrings = map alignmentToString aligns
+  captionDoc <- if null capt
+                   then return ""
+                   else do
+                      c <- inlineListToConfluence opts capt
+                      return $ "<caption>" ++ c ++ "</caption>\n"
+  let percent w = show (truncate (100*w) :: Integer) ++ "%"
+  let coltags = if all (== 0.0) widths
+                   then ""
+                   else unlines $ map
+                         (\w -> "<col width=\"" ++ percent w ++ "\" />") widths
+  head' <- if all null headers
+              then return ""
+              else do
+                 hs <- tableRowToConfluenceUsingTags opts alignStrings 0 headers
+                 return $ "<thead>\n" ++ hs ++ "\n</thead>\n"
+  body' <- zipWithM (tableRowToConfluenceUsingTags opts alignStrings) [1..] rows
+  return $ "<table>\n" ++ captionDoc ++ coltags ++ head' ++
+            "<tbody>\n" ++ unlines body' ++ "</tbody>\n</table>\n"
+
+tableToConfluenceUsingTags _ _ = undefined
+
+tableToConfluenceUsingBars :: WriterOptions -> Block -> State WriterState String
+tableToConfluenceUsingBars opts (Table [] [] [] headers rows) = do
+  hdrs <- tableRowToConfluenceUsingBars opts "||" headers
+  body <- mapM (tableRowToConfluenceUsingBars opts "|") rows
+  return $ unlines $ (hdrs : body)
+
+tableToConfluenceUsingBars _ _ = undefined
 
 -- Auxiliary functions for lists:
 
@@ -256,11 +269,10 @@ listItemToConfluence :: WriterOptions -> Char -> [Block] -> State WriterState St
 listItemToConfluence opts marker items = do
   contents <- blockListToConfluence opts items
   useTags <- get >>= return . stUseTags
+  level <- get >>= return . stListLevel
   if useTags
      then return $ "<li>" ++ contents ++ "</li>"
-     else do
-       level <- get >>= return . stListLevel
-       return $ (replicate level market) ++ " " ++ contents
+     else return $ (replicate level marker) ++ " " ++ contents
 
 -- | Convert definition list item (label, list of blocks) to Confluence.
 definitionListItemToConfluence :: WriterOptions
@@ -309,12 +321,12 @@ isPlainOrPara _         = False
 
 -- Auxiliary functions for tables:
 
-tableRowToConfluence :: WriterOptions
+tableRowToConfluenceUsingTags :: WriterOptions
                     -> [String]
                     -> Int
                     -> [[Block]]
                     -> State WriterState String
-tableRowToConfluence opts alignStrings rownum cols' = do
+tableRowToConfluenceUsingTags opts alignStrings rownum cols' = do
   let celltype = if rownum == 0 then "th" else "td"
   let rowclass = case rownum of
                       0                  -> "header"
@@ -324,6 +336,14 @@ tableRowToConfluence opts alignStrings rownum cols' = do
             (\alignment item -> tableItemToConfluence opts celltype alignment item)
             alignStrings cols'
   return $ "<tr class=\"" ++ rowclass ++ "\">\n" ++ unlines cols'' ++ "</tr>"
+
+tableRowToConfluenceUsingBars :: WriterOptions
+                    -> String
+                    -> [[Block]]
+                    -> State WriterState String
+tableRowToConfluenceUsingBars opts bars cols = do
+  contents <- mapM (blockListToConfluence opts) cols
+  return $ bars ++ (intercalate bars contents) ++ bars
 
 alignmentToString :: Alignment -> [Char]
 alignmentToString alignment = case alignment of
@@ -391,20 +411,21 @@ inlineToConfluence opts (Quoted DoubleQuote lst) = do
 inlineToConfluence opts (Cite _  lst) = inlineListToConfluence opts lst
 
 inlineToConfluence _ (Code _ str) =
-  return $ "{{" ++ str ++ "}}"
+  return $ "{code}" ++ str ++ "{code}"
 
 inlineToConfluence _ (Str str) = do
   return $ escapeString str
 
--- | Requires the LaTeX plugin: https://studio.plugins.atlassian.com/wiki/display/CLATEX/LaTeX+Plugin
+-- | Requires the LaTeX plugin:
+-- https://studio.plugins.atlassian.com/wiki/display/CLATEX/LaTeX+Plugin
 inlineToConfluence _ (Math _ str) =
-  return $ "{latex}" ++ str ++ "{latex}")
+  return $ "{latex}\\( " ++ str ++ " \\){latex}"
 
 inlineToConfluence _ (RawInline "confluence" str) = return str
 inlineToConfluence _ (RawInline "html" str) = return str
 inlineToConfluence _ (RawInline _ _) = return ""
 
-inlineToConfluence _ (LineBreak) = return "<br />\n"
+inlineToConfluence _ (LineBreak) = return "\\\\"
 
 inlineToConfluence _ Space = return " "
 
@@ -425,23 +446,26 @@ inlineToConfluence opts (Image alt (src, tit)) = do
       attrs = filter (not . null) [attrTitle, attrAlt]
   return $ "!" ++ src ++ linkAttrJoin attrs ++ "!"
 
+-- | Footnotes require the following free (after filling in survey...) plugin:
+-- http://www.adaptavist.com/display/Plugins/Footnotes
 inlineToConfluence opts (Note contents) = do
   contents' <- blockListToConfluence opts contents
   modify $ \s -> s { stHaveFootnotes = True }
   return $ "{footnote}" ++ contents' ++ "{footnote}"
 
-twoSepJoin :: Char -> Char -> [String] -> String
-twoSepJoin :: [String] -> String
-twoSepJoin c1 c2 (x:xs) = concat $ [c1,x] ++ (map (c2:) xs)
+twoSepJoin :: String -> String -> [String] -> String
+twoSepJoin c1 c2 (x:xs) = c1 ++ x ++ concat (map (c2 ++) xs)
 twoSepJoin _ _ _ = ""
 
 -- | Join a list of link attributes. First use '|' then ','
 linkAttrJoin :: [String] -> String
-linkAttrJoin = twoSepJoin '|' ','
+linkAttrJoin = twoSepJoin "|" ", "
 
 -- | Join a list of macro attributes. First use ':' then '|'
-macroAttrJoin :: [String] -> String
-macroAttrJoin = twoSepJoin ':' '|'
+--macroAttrJoin :: [String] -> String
+--macroAttrJoin = twoSepJoin ":" "|"
 
-noteNumberFormat :: Int -> String
-noteNumberFormat n = "`[" ++ (show n) ++ "]`"
+-- | Escape a double quote string using &quot;
+dqAmpEscapeString :: String -> String
+dqAmpEscapeString = escapeStringUsing [('"', "&quot;")]
+
